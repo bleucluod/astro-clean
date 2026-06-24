4b363280855eabdbbf2dfb2dd0ba3cdc82c0d059
Arad <arad@example.com>
2026-06-24 12:08:03 +0330

    Revert "Add core pattern engine insight"
    This reverts commit 1df211d46431b62f09189a28869f6321e4882c15.

--- lib/astro-engine/rules.ts
+++ lib/astro-engine/rules.ts
@@ -102,61 +102,6 @@ function findDominantModality(signs: ZodiacSymbolProfile[]): AstroModality | nul
   return (dominantModality as AstroModality | undefined) ?? null;
 }
 
-function makeCorePatternInsight(chart: EngineChartInput): EngineInsight | null {
-  const sun = chart.sun ? zodiacKnowledge[chart.sun] : null;
-  const moon = chart.moon ? zodiacKnowledge[chart.moon] : null;
-  const rising = chart.rising ? zodiacKnowledge[chart.rising] : null;
-
-  if (!sun || !moon || !rising) {
-    return null;
-  }
-
-  const sameElement =
-    sun.element === moon.element && moon.element === rising.element;
-  const sameModality =
-    sun.modality === moon.modality && moon.modality === rising.modality;
-
-  const elementLine = sameElement
-    ? "\u0647\u0631 \u0633\u0647 \u0646\u0642\u0637\u0647 \u0627\u0635\u0644\u06cc \u0631\u0648\u06cc \u0639\u0646\u0635\u0631 " +
-        elementLabels[sun.element] +
-        " \u062a\u0623\u06a9\u06cc\u062f \u062f\u0627\u0631\u0646\u062f\u061b \u0627\u06cc\u0646 \u0645\u06cc\u200c\u062a\u0648\u0627\u0646\u062f \u062f\u0631 \u062e\u0648\u0627\u0646\u0634 \u0646\u0645\u0627\u062f\u06cc\u0646 \u062d\u0633 \u0627\u0646\u0633\u062c\u0627\u0645 \u0648 \u062a\u06a9\u0631\u0627\u0631 \u06cc\u06a9 \u0632\u0628\u0627\u0646 \u062f\u0631\u0648\u0646\u06cc \u0631\u0627 \u0646\u0634\u0627\u0646 \u062f\u0647\u062f."
-    : "\u0633\u0647 \u0646\u0642\u0637\u0647 \u0627\u0635\u0644\u06cc \u0645\u06cc\u0627\u0646 " +
-        elementLabels[sun.element] +
-        "\u060c " +
-        elementLabels[moon.element] +
-        " \u0648 " +
-        elementLabels[rising.element] +
-        " \u067e\u062e\u0634 \u0634\u062f\u0647\u200c\u0627\u0646\u062f\u061b \u0627\u06cc\u0646 \u0645\u06cc\u200c\u062a\u0648\u0627\u0646\u062f \u062f\u0631 \u062e\u0648\u0627\u0646\u0634 \u0646\u0645\u0627\u062f\u06cc\u0646 \u0686\u0646\u062f\u0644\u0627\u06cc\u0647 \u0628\u0648\u062f\u0646 \u062a\u062c\u0631\u0628\u0647 \u062f\u0631\u0648\u0646\u06cc\u060c \u0627\u062d\u0633\u0627\u0633\u06cc \u0648 \u0627\u062c\u062a\u0645\u0627\u0639\u06cc \u0631\u0627 \u0646\u0634\u0627\u0646 \u062f\u0647\u062f.";
-
-  const modalityLine = sameModality
-    ? "\u0647\u0645\u0647 \u0627\u06cc\u0646 \u0646\u0642\u0627\u0637 \u06a9\u06cc\u0641\u06cc\u062a " +
-        modalityLabels[sun.modality] +
-        " \u062f\u0627\u0631\u0646\u062f\u061b \u0628\u0646\u0627\u0628\u0631\u0627\u06cc\u0646 \u0631\u06cc\u062a\u0645 \u06a9\u0644\u06cc \u0686\u0627\u0631\u062a \u0645\u06cc\u200c\u062a\u0648\u0627\u0646\u062f \u06cc\u06a9 \u0627\u0644\u06af\u0648\u06cc \u062a\u06a9\u0631\u0627\u0631\u0634\u0648\u0646\u062f\u0647 \u0648 \u0642\u0627\u0628\u0644 \u062a\u0634\u062e\u06cc\u0635 \u062f\u0627\u0634\u062a\u0647 \u0628\u0627\u0634\u062f."
-    : "\u06a9\u06cc\u0641\u06cc\u062a\u200c\u0647\u0627\u06cc \u0645\u062a\u0641\u0627\u0648\u062a \u0645\u06cc\u0627\u0646 \u062e\u0648\u0631\u0634\u06cc\u062f\u060c \u0645\u0627\u0647 \u0648 \u0637\u0627\u0644\u0639 \u0646\u0634\u0627\u0646 \u0645\u06cc\u200c\u062f\u0647\u062f \u06a9\u0647 \u0631\u0648\u0627\u06cc\u062a \u0646\u0645\u0627\u062f\u06cc\u0646 \u0686\u0627\u0631\u062a \u0641\u0642\u0637 \u06cc\u06a9 \u0631\u06cc\u062a\u0645 \u0648\u0627\u062d\u062f \u0646\u062f\u0627\u0631\u062f \u0648 \u0628\u06cc\u0646 \u0634\u0631\u0648\u0639\u060c \u062a\u062b\u0628\u06cc\u062a \u0648 \u0633\u0627\u0632\u06af\u0627\u0631\u06cc \u062d\u0631\u06a9\u062a \u0645\u06cc\u200c\u06a9\u0646\u062f.";
-
-  return {
-    id: `engine-v0-core-pattern-${sun.key}-${moon.key}-${rising.key}`,
-    category: "growth",
-    tone: "reflective",
-    title: "\u0627\u0644\u06af\u0648\u06cc \u0633\u0647\u200c\u06af\u0627\u0646\u0647 \u0686\u0627\u0631\u062a",
-    summary:
-      elementLine +
-      " " +
-      modalityLine +
-      " \u0627\u06cc\u0646 \u0628\u0631\u062f\u0627\u0634\u062a \u0641\u0642\u0637 \u06cc\u06a9 \u0646\u0642\u0634\u0647 \u0646\u0645\u0627\u062f\u06cc\u0646 \u0627\u0648\u0644\u06cc\u0647 \u0627\u0633\u062a \u0648 \u062c\u0627\u06cc \u0645\u062d\u0627\u0633\u0628\u0647 \u06a9\u0627\u0645\u0644 \u0646\u062c\u0648\u0645\u06cc \u0631\u0627 \u0646\u0645\u06cc\u200c\u06af\u06cc\u0631\u062f.",
-    keywords: [
-      "\u062e\u0648\u0631\u0634\u06cc\u062f",
-      "\u0645\u0627\u0647",
-      "\u0637\u0627\u0644\u0639",
-      "\u0627\u0644\u06af\u0648\u06cc \u0627\u0635\u0644\u06cc",
-    ],
-    weight: 70,
-    source: {
-      rule: "core-pattern",
-    },
-  };
-}
-
 function makeElementBalanceInsight(chart: EngineChartInput): EngineInsight | null {
   const signs = getChartSigns(chart);
 
@@ -221,7 +166,6 @@ export function generateStructuredInsights(chart: EngineChartInput): EngineInsig
     .map(([point, signKey]) => makePointInsight(point, signKey));
 
   const balanceInsights = [
-    makeCorePatternInsight(chart),
     makeElementBalanceInsight(chart),
     makeModalityBalanceInsight(chart),
   ].filter((insight): insight is EngineInsight => Boolean(insight));
